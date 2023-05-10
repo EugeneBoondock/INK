a05bdb05c5187ee32bd1871170f5fd2711a66e40
Eugene Boondock <philosncube@gmail.com>
2023-05-11 01:51:50 +0200

    Update spla.py

--- spla.py
+++ spla.py
@@ -1,6 +1,6 @@
 import requests
 
-api_key = "N7OWNHWblNfll0UKvQrkkB4qty4l2U_xE_miY4SZcBY"
-url = f"https://api.unsplash.com/photos/random?client_id={api_key}"
+api_key = "here"
+url = f"here"
 response = requests.get(url)
 print(response.status_code)
